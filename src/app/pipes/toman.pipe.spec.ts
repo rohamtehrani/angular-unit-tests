fbd6c2377bac3e8ce80f73dd41f50d1109bda8f3
Roham Rafiei <roham.rafii@gmail.com>
2023-10-21 02:43:21 +0330

    Add more tests to toman pipe

--- src/app/pipes/toman.pipe.spec.ts
+++ src/app/pipes/toman.pipe.spec.ts
@@ -1,19 +1,33 @@
 import { TomanPipe } from './toman.pipe';
 
 describe('TomanPipe', () => {
+  let pipe = new TomanPipe();
+
+  beforeEach(() => {
+    pipe = new TomanPipe();
+  })
+
   it('create an instance', () => {
-    const pipe = new TomanPipe();
     expect(pipe).toBeTruthy();
   });
 
   it('changes 1000 Rial to 100 Toman', () => {
-    // Arrange
-    const pipe = new TomanPipe();
+    expect(pipe.transform(1000)).toEqual(100);
+  })
 
-    //Act
-    const result = pipe.transform(1000);
+  it('change string of "1000" to 100', () => {
+    expect(pipe.transform("1000")).toEqual(100);
+  })
 
-    // Assert
-    expect(result).toEqual(100);
+  it('Round down the amount', () => {
+    expect(pipe.transform(79)).toEqual(7);
+  })
+
+  it('Doesn\'t mess with zero', () => {
+    expect(pipe.transform(0)).toEqual(0);
+  })
+
+  it('Doesn\'t mess with "0"', () => {
+    expect(pipe.transform("0")).toEqual(0);
   })
 });
