39174103c9937ba227276ebb443ab00f3cdbb535
Roham Rafiei <roham.rafii@gmail.com>
2023-10-22 20:56:32 +0330

    Add render test for hero-detail component

--- src/app/components/hero-detail/hero-detail.component.spec.ts
+++ src/app/components/hero-detail/hero-detail.component.spec.ts
@@ -5,23 +5,46 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { HeroService } from 'src/app/services/hero.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { of } from 'rxjs';
 
 describe('HeroDetailComponent', () => {
-  let component: HeroDetailComponent;
   let fixture: ComponentFixture<HeroDetailComponent>;
+  let mockActivatedRoute, mockHeroService, mockLocation;
 
   beforeEach(() => {
+    mockActivatedRoute = {
+      snapshot: { paramMap: { get: () => '3'}}
+    }
+    mockHeroService = jasmine.createSpyObj(['getHero', 'updateHero']);
+    mockLocation = jasmine.createSpyObj(['back']);
+
     TestBed.configureTestingModule({
       declarations: [HeroDetailComponent],
-      imports: [RouterTestingModule, HttpClientTestingModule, FormsModule],
-      providers: [HeroService]
+      imports: [FormsModule],
+      providers: [
+        {provide: ActivatedRoute, useValue: mockActivatedRoute},
+        {provide: HeroService, useValue: mockHeroService},
+        {provide: Location, useValue: mockLocation}
+      ]
     });
     fixture = TestBed.createComponent(HeroDetailComponent);
-    component = fixture.componentInstance;
+
+    mockHeroService.getHero.and.returnValue(of({
+      id: 3,
+      name: 'SuperDude',
+      strength: 100
+    }));
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render hero name in a h2 tag', () => {
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('h2').textContent)
+      .toContain('SUPERDUDE')
   });
+
 });
+
