2da2709f176acd863e5eed4dc2eecd5a2379e31b
Roham Rafiei <roham.rafii@gmail.com>
2023-10-22 17:24:51 +0330

    deep integration test for hero service

--- src/app/services/hero.service.spec.ts
+++ src/app/services/hero.service.spec.ts
@@ -2,21 +2,35 @@ import { TestBed } from '@angular/core/testing';
 
 import { HeroService } from './hero.service';
 
-import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 import { MessageService } from './message.service';
 
 describe('HeroService', () => {
   let service: HeroService;
+  let mockMessageservice: MessageService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
+    mockMessageservice = jasmine.createSpyObj(['add']);
+
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [MessageService, HeroService]
+      providers: [
+        HeroService,
+        {provide: MessageService, useValue: mockMessageservice}
+      ]
     });
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(HeroService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  it('should call get with the correct URL', () => {
+    service.getHero(4).subscribe();
+    service.getHero(3).subscribe();
+
+    const req = httpTestingController.expectOne('api/heroes/4');
+    req.flush({id: 4, name: 'SOME NAME', strength: 3});
+    httpTestingController.verify();
+  })
 });
+
